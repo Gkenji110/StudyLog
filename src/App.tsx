f865c43c5aca82e474cda2d70b1c93f5418917fa
JoaoGuilherme <joaoguilhermecarvalhonovaes@gmail.com>
2025-09-25 20:58:02 -0300

    feat: added study-details

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/home"
 import { NewSession } from "./pages/new-session";
 import { Layout } from "./components/layout";
+import { StudyDetails } from "./pages/study-details";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Layout/>}>
           <Route element={<Home />} index path="/" />
           <Route element={<NewSession />} index path="/add" />
+          <Route element={<StudyDetails />} index path="/study-details" />
           </Route>
         </Routes>
       </BrowserRouter>
