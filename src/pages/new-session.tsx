09f87917b3171242a1eff96b5ced27553287bb46
gkenji110 <gustavo.terada01@gmail.com>
2025-09-25 20:40:37 -0300

    feat: created new session page

--- src/pages/new-session.tsx
+++ src/pages/new-session.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+import type { Study } from "../types/study";
+import { StudyForm } from "../components/study-form";
+import { StudyList } from "../components/study-list";
+
 export function NewSession(){
+    const [studies, setStudies] = useState<Study[]>([]);
+
+
+    function newSession(study: Study): void {
+    setStudies((prev) => [...prev, study]);
+    }
+
     return (
-        <h2>Formulário para adicionar sessão</h2>
-    );
-    
+        <>
+            <h2>Adicionar novo estudo</h2>
+
+            <StudyForm onAdd={newSession} />
+            
+            <StudyList studyList={studies} />
+        </>
+    )
+
 }
